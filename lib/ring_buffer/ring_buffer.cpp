abc9dcd1fd7a94eb0e3a6eb27c87e030093cbd28
Dávid Kovács <david.kovacs@vacuumlabs.com>
2022-10-18 21:45:39 +0200

    Optimize ring_buffer

--- lib/ring_buffer/ring_buffer.cpp
+++ lib/ring_buffer/ring_buffer.cpp
@@ -1,6 +1,16 @@
 #include <util/atomic.h>
 #include "ring_buffer.h"
 
+// ProMicro does not do division in HW, so '%' is expensive
+inline uint8_t increment_index(uint8_t current_value) {
+    uint8_t new_value = current_value + 1;
+    if (new_value >= BUFFER_LENGTH) {
+        return 0;
+    } else {
+        return new_value;
+    }
+}
+
 RingBuffer::RingBuffer() {
     for (uint8_t i = 0; i < BUFFER_LENGTH; i++) {
         this->buffer[i] = 0x00;
@@ -16,14 +26,14 @@ void RingBuffer::append_elem(uint8_t elem) {
         return;
     }
     this->buffer[ this->write_index ] = elem;
-    this->write_index = (this->write_index + 1) % BUFFER_LENGTH;
+    this->write_index = increment_index(this->write_index);
     this->size++;
 }
 
 // Called from main loop
 uint8_t RingBuffer::get_next_elem() {
     uint8_t elem = this->buffer[ this->read_index ];
-    this->read_index = (this->read_index + 1) % BUFFER_LENGTH;
+    this->read_index = increment_index(this->read_index);
     ATOMIC_BLOCK(ATOMIC_FORCEON) {
         this->size--;
     }
