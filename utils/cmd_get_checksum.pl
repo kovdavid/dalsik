c306475d6b00ce51c2572f822cbbbfc60b676395
Dávid Kovács <david.kovacs@vacuumlabs.com>
2023-04-03 19:50:26 +0200

    Remove old `utils/` scripts

--- utils/cmd_get_checksum.pl
+++ utils/cmd_get_checksum.pl
@@ -2,26 +2,50 @@
 
 use strict;
 use warnings;
-use FindBin qw[ $Bin ];
 use Getopt::Long qw( GetOptions );
 
-use lib "$Bin";
-
-use Dalsik;
-
 my $serial = '/dev/ttyACM0';
-GetOptions("serial=s" => \$serial);
+my $clear_cmd_buffer = 0;
+GetOptions({
+    "serial=s" => \$serial,
+    "clear=c" => \$clear_cmd_buffer,
+});
 
-my $fh = Dalsik::open_serial($serial);
+my $fh = open_serial($serial);
 
-my $cmd = Dalsik::get_cmd('GET_CHECKSUM');
-unless (print $fh $cmd) {
-    die "Could not send PING: $!";
+my $cmd = "DALSIK-\x01";
+
+if ($clear_cmd_buffer) {
+    for my $iteration (1..32) {
+        unless (print $fh $cmd) {
+            die "Iteration $iteration: Could not send command: $!";
+        }
+        my $res = <$fh>;
+        if (length($res || "") == 7) {
+            print "cmd_buffer cleared; exiting\n";
+            exit;
+        }
+
+        print $fh "\x00";
+    }
+} else {
+    unless (print $fh $cmd) {
+        die "Could not send command: $!";
+    }
+
+    my $res = <$fh>;
+    my $cmd_res = <$fh>;
+
+    print "$res";
 }
 
-my $res = <$fh>;
-my $cmd_res = <$fh>;
-
-print "$res";
-
 close $fh;
+
+sub open_serial {
+    my ($serial) = @_;
+
+    `stty -F $serial 9600 raw -echo`;
+    open my $fh, "+<:raw", $serial or die "Could not open $serial";
+
+    return $fh;
+}
