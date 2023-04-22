e60326dcb897d0d2c38d267fcfe88ad5df07021f
Dávid Kovács <kovdavid@gmail.com>
2023-04-22 10:18:12 +0200

    Move KeyInfo implementation into '.cpp' file

--- lib/key_info/key_info.h
+++ lib/key_info/key_info.h
@@ -12,108 +12,22 @@ class KeyInfo {
         uint8_t key;
         KeyCoords coords;
 
-        KeyInfo() {
-            this->type = KEY_NORMAL;
-            this->layer = 0;
-            this->mod = MOD_CLEAR;
-            this->key = KC_NO;
-            this->coords = KeyCoords { COORD_UNKNOWN, COORD_UNKNOWN };
-        }
-        KeyInfo(KeyCoords c) {
-            this->type = KEY_NORMAL;
-            this->layer = 0;
-            this->mod = MOD_CLEAR;
-            this->key = KC_NO;
-            this->coords = c;
-        }
-        KeyInfo(uint32_t progmem_data, KeyCoords c) {
-            this->type  = (progmem_data >> 24) & 0xFF;
-            this->layer = (progmem_data >> 16) & 0xFF;
-            this->mod   = (progmem_data >> 8)  & 0xFF;
-            this->key   = (progmem_data >> 0)  & 0xFF;
-            this->coords = c;
-        }
-        KeyInfo(uint8_t type, uint8_t layer, uint8_t mod, uint8_t key, KeyCoords c) {
-            this->type = type;
-            this->layer = layer;
-            this->mod = mod;
-            this->key = key;
-            this->coords = c;
-        }
+        KeyInfo(void);
+        KeyInfo(KeyCoords c);
+        KeyInfo(uint32_t progmem_data, KeyCoords c);
+        KeyInfo(uint8_t type, uint8_t layer, uint8_t mod, uint8_t key, KeyCoords c);
 
-        KeyInfo use_mod() {
-            return KeyInfo(this->type, 0, this->mod, KC_NO, this->coords);
-        }
+        KeyInfo use_mod();
+        KeyInfo use_key();
 
-        KeyInfo use_key() {
-            return KeyInfo(this->type, 0, MOD_CLEAR, this->key, this->coords);
-        }
+        void print_internal_state();
 
-        bool equals(KeyInfo other) {
-            return
-                this->type == other.type
-                && this->layer == other.layer
-                && this->mod == other.mod
-                && this->key == other.key
-                && this->coords.equals(other.coords);
-        }
-
-        void print_internal_state() {
-            Serial.print(">> KEY_INFO ROW:");
-            Serial.print(this->coords.row);
-            Serial.print(" COL:");
-            Serial.print(this->coords.col);
-            Serial.print(" TYPE:");
-            Serial.print(this->type);
-            Serial.print(" LAYER:");
-            Serial.print(this->layer);
-            Serial.print(" MOD:");
-            Serial.print(this->mod);
-            Serial.print(" KEY:");
-            Serial.print(this->key);
-            Serial.print("\n");
-        }
-
-        uint8_t skip_layer_reload() {
-            return
-                this->coords.row == COORD_COMBO
-                || this->coords.row == COORD_UNKNOWN
-                || this->coords.col == COORD_UNKNOWN;
-        }
-
-        uint8_t is_any_dual_mod_key() {
-            return
-                this->type == KEY_DUAL_MOD
-                || this->type == KEY_SOLO_DUAL_MOD
-                || this->type == KEY_TIMED_DUAL_MOD
-                || this->type == KEY_DUAL_DTH_MOD;
-        }
-        uint8_t is_any_timed_dual_key() {
-            return
-                this->type == KEY_TIMED_DUAL_MOD
-                || this->type == KEY_TIMED_DUAL_LAYER;
-        }
-        uint8_t is_any_dual_layer_key() {
-            return
-                this->type == KEY_DUAL_LAYER
-                || this->type == KEY_SOLO_DUAL_LAYER
-                || this->type == KEY_TIMED_DUAL_LAYER
-                || this->type == KEY_DUAL_DTH_LAYER;
-        }
-        uint8_t is_any_dual_key() {
-            return
-                this->is_any_dual_mod_key()
-                || this->is_any_dual_layer_key();
-        }
-        uint8_t is_any_solo_dual_key() {
-            return
-                this->type == KEY_SOLO_DUAL_MOD
-                || this->type == KEY_SOLO_DUAL_LAYER;
-        }
-        uint8_t is_multimedia_key() {
-            return
-                this->type == KEY_MULTIMEDIA_0
-                || this->type == KEY_MULTIMEDIA_1
-                || this->type == KEY_MULTIMEDIA_2;
-        }
+        bool equals(KeyInfo other);
+        bool skip_layer_reload();
+        bool is_any_dual_mod_key();
+        bool is_any_timed_dual_key();
+        bool is_any_dual_layer_key();
+        bool is_any_dual_key();
+        bool is_any_solo_dual_key();
+        bool is_multimedia_key();
 };
