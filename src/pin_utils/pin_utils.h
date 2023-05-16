7d8902c3e5a4480311ddda6ca7faeb3aad114cfc
Dávid Kovács <kovdavid@gmail.com>
2023-05-16 18:47:16 +0200

    Make inline functions static in pin_utils

--- src/pin_utils/pin_utils.h
+++ src/pin_utils/pin_utils.h
@@ -5,30 +5,30 @@
 #include "keyboard_hardware.h"
 
 namespace PinUtils {
-    inline void set_output_high(uint8_t pin) {
+    static inline void set_output_high(uint8_t pin) {
         _SFR_IO8((pin >> 4) + 2) |= _BV(pin & 0x0F); // HIGH
     }
 
-    inline void set_output_low(uint8_t pin) {
+    static inline void set_output_low(uint8_t pin) {
         _SFR_IO8((pin >> 4) + 2) &= ~_BV(pin & 0x0F); // LOW
     }
 
-    inline void pinmode_input_pullup(uint8_t pin) {
+    static inline void pinmode_input_pullup(uint8_t pin) {
         _SFR_IO8((pin >> 4) + 1) &= ~_BV(pin & 0x0F); // INPUT
         _SFR_IO8((pin >> 4) + 2) |=  _BV(pin & 0x0F); // HIGH
     }
 
-    inline void pinmode_output_low(uint8_t pin) {
+    static inline void pinmode_output_low(uint8_t pin) {
         _SFR_IO8((pin >> 4) + 1) |=  _BV(pin & 0x0F); // OUTPUT
         PinUtils::set_output_low(pin);
     }
 
-    inline void pinmode_output_high(uint8_t pin) {
+    static inline void pinmode_output_high(uint8_t pin) {
         _SFR_IO8((pin >> 4) + 1) |= _BV(pin & 0x0F); // OUTPUT
         PinUtils::set_output_high(pin);
     }
 
-    inline uint8_t read_pin(uint8_t pin) {
+    static inline uint8_t read_pin(uint8_t pin) {
         return (_SFR_IO8(pin >> 4) & _BV(pin & 0x0F));
     }
 }
